a737beb9147c4444138113ea761423fd0f5b4fce
hahaskymark <hahaskymark@gmail.com>
2024-04-07 21:37:23 +0800

    second commit

---
+++ index.js
@@ -0,0 +1,5 @@
+const random = require('./random'); // Importing the module
+
+// Example usage
+const randomNumber = random(1, 100);
+console.log(randomNumber); // This will print a random number between 1 and 100
